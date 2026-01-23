ae15c1d28a422f437180d5eb70a9e429117b99c4
cobi <js3104@nyu.edu>
2026-01-23 12:54:12 +0200

    initial commit

---
+++ clock01.py
@@ -0,0 +1,59 @@
+from tkinter import *
+import time
+
+
+class StopWatch(Frame):
+    """ Implements a stop watch frame widget. """
+
+    def __init__(self, parent=None, **kw):
+        Frame.__init__(self, parent, kw)
+        self._start = 0.0
+        self._elapsedtime = 0.0
+        self._running = 0
+        self.timestr = StringVar()
+        self.makeWidgets()
+
+    def makeWidgets(self):
+        """ Make the time label. """
+        l = Label(self, textvariable=self.timestr, bg='yellow')
+        self._setTime(self._elapsedtime)
+        l.pack(fill=X, expand=NO, pady=2, padx=2)
+
+    def _update(self):
+        """ Update the label with elapsed time. """
+        self._elapsedtime = time.time() - self._start
+        self._setTime(self._elapsedtime)
+        self._timer = self.after(50, self._update)
+
+    def _setTime(self, elap):
+        """ Set the time string to Minutes:Seconds:Hundreths """
+        minutes = int(elap / 60)
+        seconds = int(elap - minutes * 60.0)
+        hseconds = int((elap - minutes * 60.0 - seconds) * 100)
+        self.timestr.set('%02d:%02d:%02d' % (minutes, seconds, hseconds))
+
+    def Start(self):
+        """ Start the stopwatch, ignore if running. """
+        if not self._running:
+            self._start = time.time() - self._elapsedtime
+            self._update()
+            self._running = 1
+
+
+def main():
+    root = Tk()
+    root.geometry('300x300')
+    bottom_frame = Frame(root)
+    bottom_frame.pack(side=BOTTOM)  # which implicitly packs top_frame on the top
+    sw = StopWatch(root)
+    sw.pack(side=TOP)
+    # Button(root, text='Start', command=sw.Start).pack(side=LEFT)
+    sw.Start()
+
+    Button(root, text='Quit', command=root.quit).pack(side=RIGHT)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
